8cd451d428b783168ad03d3051200c0d0aad6db0
Joppe Aarts <joppe@apestaartje.info>
2021-06-25 09:41:47 +0200

    Show test with interval after clicking start

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Interval } from '../component/interval';
 
 export default function Test() {
+    const [show, setShow] = useState(false);
+
+    function handleClick() {
+        setShow(!show);
+    }
+
     return (
         <section>
             <h1>What will it do?</h1>
-            <Interval />
+
+            {!show && (
+                <button type="button" onClick={handleClick}>
+                    Start
+                </button>
+            )}
+            {show && <Interval />}
         </section>
     );
 }
